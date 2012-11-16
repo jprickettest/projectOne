35448f34f874ffa21620d01acc6a81c8cf8798fa
JasonLoy <jpricket@microsoft.com>
2012-11-16 08:02:53 -0500

    no comment

---
+++ Solutions/SimpleApp/SimpleApp/SimpleMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleApp
+{
+    public static class SimpleMath
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Solutions/SimpleApp/UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleApp;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(8, SimpleMath.Add(1, 7));
+        }
+    }
+}
